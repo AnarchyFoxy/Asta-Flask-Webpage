f13d0ef162016af31f86184d11c4087a3ef67dd8
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-03 21:39:32 +0100

    Added comments

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 2023))
     app.run(debug=True, host="0.0.0.0", port = port)
 
-# flask --app app run
-# gunicorn --bind=0.0.0.0:2023 app:app
+# flask --app app run develop server
+# gunicorn --bind=0.0.0.0:2023 app:app production server
